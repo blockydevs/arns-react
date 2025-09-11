a29e1ed59bd850fc84253983160500876662fa53
Mateusz Domański <mateusz.domanski@blockydevs.com>
2025-09-11 14:45:57 +0200

    feat: basic SearchListingByName

--- src/components/pages/Listings/CompletedListingsTab.tsx
+++ src/components/pages/Listings/CompletedListingsTab.tsx
@@ -15,7 +15,7 @@ import {
 import { useQuery } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 
-const PAGE_SIZE = 10;
+const PAGE_SIZE = 20;
 
 const CompletedListingsTab = () => {
   const navigate = useNavigate();
